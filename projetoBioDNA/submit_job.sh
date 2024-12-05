7189b6d830d60743a064752003cba9ec47b4720d
pedrocivita <pedro.civita@gmail.com>
2024-12-04 22:31:09 -0300

    Projetos Finalizado

--- projetoBioDNA/submit_job.sh
+++ projetoBioDNA/submit_job.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
-#SBATCH --job-name=clique_project    # Nome do job
-#SBATCH --output=output_%x_%j.txt    # Arquivo de saída baseado no nome do job e ID
-#SBATCH --ntasks=4                   # Número total de tarefas (processos MPI)
-#SBATCH --cpus-per-task=1            # Número de CPUs por tarefa
-#SBATCH --mem=4096                   # Memória alocada (4GB)
-#SBATCH --time=03:00:00              # Tempo máximo de execução
-#SBATCH --partition=espec            # Partição utilizada
+#SBATCH --job-name=mpi_exercises
+#SBATCH --output=output_%x_%j.txt
+#SBATCH --ntasks=4
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=4096
+#SBATCH --time=03:00:00
+#SBATCH --partition=espec
 
-# Carrega os módulos necessários
+# module load mpi
 # module load gcc
-# module load openmpi
 
-# Compila os programas
-# mpic++ clique_mpi.cpp -o clique_mpi
-# g++ -fopenmp clique_openmp.cpp -o clique_openmp
-# g++ clique_exaustiva.cpp -o clique_exaustiva
+# Compilação
+# mpic++ -fopenmp ex1.cpp -o ex1
+# mpic++ -fopenmp ex2.cpp -o ex2
+# mpic++ -fopenmp ex3.cpp -o ex3
+# mpic++ -fopenmp ex4.cpp -o ex4
 
-# Executa a implementação com MPI
-mpirun -np $SLURM_NTASKS ./clique_mpi > output_clique_mpi_$SLURM_JOB_ID.txt
-
-# Executa a implementação com OpenMP
-./clique_openmp > output_clique_openmp_$SLURM_JOB_ID.txt
-
-# Executa a implementação exaustiva
-./clique_exaustiva > output_clique_exaustiva_$SLURM_JOB_ID.txt
+# Execução
+mpirun -np $SLURM_NTASKS ./ex1 > output_ex1_$SLURM_JOB_ID.txt
+mpirun -np $SLURM_NTASKS ./ex2 > output_ex2_$SLURM_JOB_ID.txt
+mpirun -np $SLURM_NTASKS ./ex3 > output_ex3_$SLURM_JOB_ID.txt
+mpirun -np $SLURM_NTASKS ./ex4 > output_ex4_$SLURM_JOB_ID.txt
